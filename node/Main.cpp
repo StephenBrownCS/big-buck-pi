91d6c495793202ff474420b53ae0ddb415b6797b
Stephen Brown <StephenBrownCS@gmail.com>
2013-11-21 15:26:41 -0600

    Added new way to get own ip address

--- node/Main.cpp
+++ node/Main.cpp
@@ -13,6 +13,16 @@
 
 #include <iostream>
 #include <sstream>
+#include <sys/types.h>
+#include <ifaddrs.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netpacket/packet.h>
+#include <net/ethernet.h>
 #include "SendingSocket.h"
 #include "Packet.h"
 #include "UDPPacket.h"
@@ -31,6 +41,7 @@ const int OWN_LISTEN_PORT = 8888;
 const char* NAME_SERVER_NAME = "cedar.cs.wisc.edu";
 
 unsigned short registerWithNameServer(HostAndPort & self, HostAndPort & masterHap );
+unsigned long getOwnWlanIpAddress();
 
 int main(int argc, char** argv){
     const char* destIpAddress = 0;
@@ -46,12 +57,11 @@ int main(int argc, char** argv){
     }
     */
     
-    HostAndPort self(getOwnIPAddress(), OWN_LISTEN_PORT);
+    HostAndPort self(getOwnWlanIpAddress(), OWN_LISTEN_PORT);
     HostAndPort masterHap;
     
-    unsigned int ownNodeId = registerWithNameServer(self, masterHap);
-    
     try{    
+        unsigned int ownNodeId = registerWithNameServer(self, masterHap);
         Communicator* communicator = 
             WifiCommunicator::create( 
                 masterHap.getIPAsStr().c_str(), masterHap.getPort(), OWN_LISTEN_PORT
@@ -134,4 +144,54 @@ unsigned short registerWithNameServer(HostAndPort & self, HostAndPort & masterHa
     
     return ownNodeId;   
 }
+
+unsigned long getOwnWlanIpAddress(){
+    struct ifaddrs *ifaddr, *ifa;
+   int family = 0;
+   int s = 0;
+   char host[NI_MAXHOST];
+
+   if (getifaddrs(&ifaddr) == -1) {
+       perror("getifaddrs");
+       exit(EXIT_FAILURE);
+   }
+
+   /* Walk through linked list, maintaining head pointer so we
+      can free list later */
+
+   for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+       if (ifa->ifa_addr == NULL)
+           continue;
+
+       family = ifa->ifa_addr->sa_family;
+
+       /* Display interface name and family (including symbolic
+          form of the latter for the common families) */
+
+       printf("%s  address family: %d%s\n",
+               ifa->ifa_name, family,
+               (family == AF_PACKET) ? " (AF_PACKET)" :
+               (family == AF_INET) ?   " (AF_INET)" :
+               (family == AF_INET6) ?  " (AF_INET6)" : "");
+
+       /* For an AF_INET* interface address, display the address */
+
+       if (family == AF_INET || family == AF_INET6) {
+           s = getnameinfo(ifa->ifa_addr,
+                   (family == AF_INET) ? sizeof(struct sockaddr_in) :
+                                         sizeof(struct sockaddr_in6),
+                   host, NI_MAXHOST, NULL, 0, NI_NUMERICHOST);
+           if (s != 0) {
+               printf("getnameinfo() failed: %s\n", gai_strerror(s));
+               exit(EXIT_FAILURE);
+           }
+           printf("\taddress: <%s>\n", host);
+       }
+   }
+
+   freeifaddrs(ifaddr);
+   return s;
+}
+
+
 
