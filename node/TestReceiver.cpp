d0bf9ba6958bb3f18161035f287d71b18e24f18a
Stephen Brown <StephenBrownCS@gmail.com>
2013-11-20 23:20:23 -0600

    Changed test receiver to emulate a master node.

--- node/TestReceiver.cpp
+++ node/TestReceiver.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <arpa/inet.h>
 #include "ListeningSocket.h"
+#include "SendingSocket.h"
 #include "Packet.h"
 #include "UDPPacket.h"
 #include "BigBuckPacket.h"
 #include "Utility.h"
 #include "HostAndPort.h"
+#include "PacketConstants.h"
+
+using namespace std;
+
+const int OWN_LISTEN_PORT = 8888;
+const char* NAME_SERVER_NAME = "cedar.cs.wisc.edu";
+
 //#include <chrono>
 //#include <thread>
 
@@ -14,11 +22,30 @@
 
 using namespace std;
 
-const int PORT = 55019;
-
 int main(){
     try{
-        ListeningSocket listenSock( PORT );
+        HostAndPort self(getOwnIPAddress(), OWN_LISTEN_PORT);
+    
+        // Register with the name server
+        unsigned long nameServerIp = ntohl(getIPAddressForHostname(NAME_SERVER_NAME));
+        unsigned short nameServerPort = OWN_LISTEN_PORT;
+    
+        cout << "Name Server IP: " << convertIntToIPAddressString(nameServerIp) << endl;
+    
+        HostAndPort nameServerHap(nameServerIp, nameServerPort);
+    
+        SendingSocket sock(nameServerIp, nameServerPort);
+        BigBuckPacket* registrationPkt = 
+        BigBuckPacket::create(
+            'M', DEFAULT_NODE_ID, DEFAULT_NODE_ID, NO_SEQUENCE, NO_PAYLOAD, EMPTY_PAYLOAD
+        );
+        sock.sendPacket(
+        UDPPacket::create(
+            self, nameServerHap, registrationPkt->c_str_length(), registrationPkt->c_str()
+            )
+        );    
+    
+        ListeningSocket listenSock( OWN_LISTEN_PORT );
         
         unsigned int ownIPAddress = ntohl(getOwnIPAddress());
         cout << "Own IP Address: " << convertIntToIPAddressString(ownIPAddress) << endl;
