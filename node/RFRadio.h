0af16549c6fd79025c0e0a7613ed658a96092cee
Stephen Brown <StephenBrownCS@gmail.com>
2013-12-03 17:24:37 -0600

    Initial checkin for multi-threaded RFRadio module, and changes to BigBuckSensingNode to use the radios.

---
+++ node/RFRadio.h
@@ -0,0 +1,25 @@
+#ifndef RF_RADIO_H_
+#define RF_RADIO_H_
+
+// Forward Reference
+class gnublin_gpio;
+
+class RFRadio{
+public:
+    static RFRadio* create();
+    ~RFRadio;
+    void sendPulse();
+    bool pulseDetected();
+private:
+    gnublin_gpio* gpio;
+    bool pulseDetectedRecently;
+    pthread_mutex_t* pulseFlagLock;
+    
+    RFRadio();
+    
+    // No copy or assignment
+    RFRadio( const RFRadio & );
+    RFRadio & operator=(const RFRadio & );
+};
+
+#endif
