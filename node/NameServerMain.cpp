0167d0afea2191479a0f40c782f635eeb76d8c26
Stephen Brown <StephenBrownCS@gmail.com>
2013-12-05 16:28:14 -0600

    Name Server should be robust to wifi failure.

--- node/NameServerMain.cpp
+++ node/NameServerMain.cpp
@@ -22,6 +22,7 @@
 using namespace std;
 
 const int POLLING_RATE_IN_SECONDS = 0.2; // so we don't do a busy wait
+const int RETRY_RATE_IN_SECONDS = 10; // sleep time between retries for when wifi goes down
 
 // Sends the Host and Port of the Master to the node with the specified id
 void sendMasterHapToNode( HostAndPort & self, HostAndPort & destHap, HostAndPort & masterHap, unsigned short nodeId);
@@ -35,146 +36,150 @@ int acquireNodeId( unsigned short & nextNodeIdToAssign,
 
 
 int main(int argc, char** argv){
-    try{    
-        const char* destIpAddress = 0;
-        unsigned short destPort = 0;
-        /*
-        if ( argc != 3 ){
-            cout << "Usage: Main <Dot-Separated IP Address> <port>" << endl;
-            exit( 0 );
-        }
-        else{
-            destIpAddress = argv[1];
-            destPort = atoi(argv[2]);
-        }
-        */
-    
-        // This is kind of hacky - but we identify if we're a dns-finable host (such as 
-        // cedar.cs.wisc.edu) by asking getOwnHostName(). Otherwise, fall back to 
-        // our wlan0 address.
-        unsigned long ownIPAddress = 0;
-        if (getOwnHostName() == "cedar"){
-            ownIPAddress = htonl(getOwnIPAddress());
-        }
-        else{
-            ownIPAddress = getOwnWlanIpAddress();
-        }
-        HostAndPort self(ownIPAddress, NAME_SERVER_PORT);
-        HostAndPort masterHap;
-        bool masterIsKnown = false;
-    
-        cout << "Own Hap: " << self << endl;
-    
-        // Always listen on port 8888
-        ListeningSocket listenSock( NAME_SERVER_PORT );
+    while( true ){ 
 
-        map<HostAndPort, unsigned short> sensorNodes;
-        unsigned short nextNodeIdToAssign = 1;
-    
-    
-        //loop to receive and forward packets
-        while(1){
-            // SEE IF PACKET IS AVAILABLE
-            if(listenSock.isPacketWaiting()){               
-                UDPPacket* incomingPkt = listenSock.receivePacket(0.1);
-                incomingPkt->print();
-                
-                BigBuckPacket* innerPkt = BigBuckPacket::create(incomingPkt->getPayload());
-
-                unsigned short senderId = innerPkt->getSrcNodeId();
-                HostAndPort senderHap = incomingPkt->getSrc();
-                
-                cout << "Packet Received!" << endl;
-                
-                if (innerPkt->getPacketType() == PKT_LETTER_REGISTRATION){
-                    cout << "Registration!" << endl;
-                    
-                    unsigned short nodeId = acquireNodeId( nextNodeIdToAssign, sensorNodes, senderHap);
-                    cout << "Node Id is " << nodeId << endl;
-                    
-                    // Respond with node Id
-                    SendingSocket sock(htonl(senderHap.getIP()), senderHap.getPort());
-                    cout << "Sending response to " << senderHap << endl;
-
-                    // Send the response and clean up
-                    BigBuckPacket* responseInnerPkt = BigBuckPacket::create( 
-                        'A', DEFAULT_NODE_ID, nodeId, NO_SEQUENCE, NO_PAYLOAD, EMPTY_PAYLOAD );
-                    Packet* responseOuterPkt = UDPPacket::create(
-                        self, senderHap, responseInnerPkt->c_str_length(), responseInnerPkt->c_str());
-                        
-                    sock.sendPacket(responseOuterPkt);
-                    delete responseInnerPkt;
-                    delete responseOuterPkt;
-                    
-                    if ( masterIsKnown ){
-                        sendMasterHapToNode( self, senderHap, masterHap, nodeId);
-                    } 
-                }
-                else if(innerPkt->getPacketType() == PKT_LETTER_HEARTBEAT){
-                    cout << "Heartbeat!" << endl;
-                    cout << "\tNode Id: " << senderId << endl;
-                    cout << "\tNode Hap: " << senderHap << endl;
-                }
-                else if(innerPkt->getPacketType() == PKT_LETTER_MASTER){
-                    cout << "Master Registration!" << endl;
-                    cout << "\tNode Id: " << senderId << endl;
-                    cout << "\tNode Hap: " << senderHap << endl;
-                    
-                    masterHap = senderHap;
-                    masterIsKnown = true;
-
-                    // Contact all of the nodes and inform them of the master's 
-                    // location
-                    typedef map<HostAndPort, unsigned short>::iterator HapIter;
-                    for( HapIter it = sensorNodes.begin(); it != sensorNodes.end(); ++it){
-                        unsigned short nodeId = it->second;
-                        HostAndPort hap = it->first;
-                        
-                        sendMasterHapToNode( self, hap, senderHap, nodeId );
-                    }
-                    
-                    // Send ACK to master
-                    SendingSocket sock(htonl(senderHap.getIP()), senderHap.getPort());
-                    BigBuckPacket* responseInnerPkt = BigBuckPacket::create( 
-                        'A', DEFAULT_NODE_ID, DEFAULT_NODE_ID, NO_SEQUENCE, NO_PAYLOAD, EMPTY_PAYLOAD );
-                    Packet* responseOuterPkt = UDPPacket::create(
-                        self, senderHap, responseInnerPkt->c_str_length(), responseInnerPkt->c_str());
-                    sock.sendPacket(responseOuterPkt);
-                }
-                else if(innerPkt->getPacketType() == PKT_LETTER_INFO){
-                    cout << "Info Request!" << endl;
-                    cout << "\tNode Id: " << senderId << endl;
-                    cout << "\tNode Hap: " << senderHap << endl;
-                    
-                    // Construct payload full of our own Hap, as well as
-                    // the Haps of all the other nodes
-                    ostringstream oss;
-                    oss << "Own Hap: " << self << endl;
-                    typedef map<HostAndPort, unsigned short>::iterator HapIter;
-                    for( HapIter it = sensorNodes.begin(); it != sensorNodes.end(); ++it){
-                        unsigned short nodeId = it->second;
-                        HostAndPort hap = it->first;
-                        
-                        oss << "Node: " << nodeId << " " << hap << endl;
-                    }
-                    
-                    const char* payload = oss.str().c_str();
-                    
-                    SendingSocket sock(htonl(senderHap.getIP()), senderHap.getPort());
-                    BigBuckPacket* responseInnerPkt = BigBuckPacket::create( 
-                        'A', DEFAULT_NODE_ID, DEFAULT_NODE_ID, NO_SEQUENCE, strlen(payload) + 1, payload );
-                    Packet* responseOuterPkt = UDPPacket::create(
-                        self, senderHap, responseInnerPkt->c_str_length(), responseInnerPkt->c_str());
-                    sock.sendPacket(responseOuterPkt);
-                }
+        try{    
+            const char* destIpAddress = 0;
+            unsigned short destPort = 0;
+            /*
+            if ( argc != 3 ){
+                cout << "Usage: Main <Dot-Separated IP Address> <port>" << endl;
+                exit( 0 );
             }
+            else{
+                destIpAddress = argv[1];
+                destPort = atoi(argv[2]);
+            }
+            */
+    
+            // This is kind of hacky - but we identify if we're a dns-finable host (such as 
+            // cedar.cs.wisc.edu) by asking getOwnHostName(). Otherwise, fall back to 
+            // our wlan0 address.
+            unsigned long ownIPAddress = 0;
+            if (getOwnHostName() == "cedar"){
+                ownIPAddress = htonl(getOwnIPAddress());
+            }
+            else{
+                ownIPAddress = getOwnWlanIpAddress();
+            }
+            HostAndPort self(ownIPAddress, NAME_SERVER_PORT);
+            HostAndPort masterHap;
+            bool masterIsKnown = false;
+    
+            cout << "Own Hap: " << self << endl;
+    
+            // Always listen on port 8888
+            ListeningSocket listenSock( NAME_SERVER_PORT );
+
+            map<HostAndPort, unsigned short> sensorNodes;
+            unsigned short nextNodeIdToAssign = 1;
+    
+    
+            //loop to receive and forward packets
+            while(1){
+                // SEE IF PACKET IS AVAILABLE
+                if(listenSock.isPacketWaiting()){               
+                    UDPPacket* incomingPkt = listenSock.receivePacket(0.1);
+                    incomingPkt->print();
+                
+                    BigBuckPacket* innerPkt = BigBuckPacket::create(incomingPkt->getPayload());
+
+                    unsigned short senderId = innerPkt->getSrcNodeId();
+                    HostAndPort senderHap = incomingPkt->getSrc();
+                
+                    cout << "Packet Received!" << endl;
+                
+                    if (innerPkt->getPacketType() == PKT_LETTER_REGISTRATION){
+                        cout << "Registration!" << endl;
+                    
+                        unsigned short nodeId = acquireNodeId( nextNodeIdToAssign, sensorNodes, senderHap);
+                        cout << "Node Id is " << nodeId << endl;
+                    
+                        // Respond with node Id
+                        SendingSocket sock(htonl(senderHap.getIP()), senderHap.getPort());
+                        cout << "Sending response to " << senderHap << endl;
+
+                        // Send the response and clean up
+                        BigBuckPacket* responseInnerPkt = BigBuckPacket::create( 
+                            'A', DEFAULT_NODE_ID, nodeId, NO_SEQUENCE, NO_PAYLOAD, EMPTY_PAYLOAD );
+                        Packet* responseOuterPkt = UDPPacket::create(
+                            self, senderHap, responseInnerPkt->c_str_length(), responseInnerPkt->c_str());
+                        
+                        sock.sendPacket(responseOuterPkt);
+                        delete responseInnerPkt;
+                        delete responseOuterPkt;
+                    
+                        if ( masterIsKnown ){
+                            sendMasterHapToNode( self, senderHap, masterHap, nodeId);
+                        } 
+                    }
+                    else if(innerPkt->getPacketType() == PKT_LETTER_HEARTBEAT){
+                        cout << "Heartbeat!" << endl;
+                        cout << "\tNode Id: " << senderId << endl;
+                        cout << "\tNode Hap: " << senderHap << endl;
+                    }
+                    else if(innerPkt->getPacketType() == PKT_LETTER_MASTER){
+                        cout << "Master Registration!" << endl;
+                        cout << "\tNode Id: " << senderId << endl;
+                        cout << "\tNode Hap: " << senderHap << endl;
+                    
+                        masterHap = senderHap;
+                        masterIsKnown = true;
+
+                        // Contact all of the nodes and inform them of the master's 
+                        // location
+                        typedef map<HostAndPort, unsigned short>::iterator HapIter;
+                        for( HapIter it = sensorNodes.begin(); it != sensorNodes.end(); ++it){
+                            unsigned short nodeId = it->second;
+                            HostAndPort hap = it->first;
+                        
+                            sendMasterHapToNode( self, hap, senderHap, nodeId );
+                        }
+                    
+                        // Send ACK to master
+                        SendingSocket sock(htonl(senderHap.getIP()), senderHap.getPort());
+                        BigBuckPacket* responseInnerPkt = BigBuckPacket::create( 
+                            'A', DEFAULT_NODE_ID, DEFAULT_NODE_ID, NO_SEQUENCE, NO_PAYLOAD, EMPTY_PAYLOAD );
+                        Packet* responseOuterPkt = UDPPacket::create(
+                            self, senderHap, responseInnerPkt->c_str_length(), responseInnerPkt->c_str());
+                        sock.sendPacket(responseOuterPkt);
+                    }
+                    else if(innerPkt->getPacketType() == PKT_LETTER_INFO){
+                        cout << "Info Request!" << endl;
+                        cout << "\tNode Id: " << senderId << endl;
+                        cout << "\tNode Hap: " << senderHap << endl;
+                    
+                        // Construct payload full of our own Hap, as well as
+                        // the Haps of all the other nodes
+                        ostringstream oss;
+                        oss << "Own Hap: " << self << endl;
+                        typedef map<HostAndPort, unsigned short>::iterator HapIter;
+                        for( HapIter it = sensorNodes.begin(); it != sensorNodes.end(); ++it){
+                            unsigned short nodeId = it->second;
+                            HostAndPort hap = it->first;
+                        
+                            oss << "Node: " << nodeId << " " << hap << endl;
+                        }
+                    
+                        const char* payload = oss.str().c_str();
+                    
+                        SendingSocket sock(htonl(senderHap.getIP()), senderHap.getPort());
+                        BigBuckPacket* responseInnerPkt = BigBuckPacket::create( 
+                            'A', DEFAULT_NODE_ID, DEFAULT_NODE_ID, NO_SEQUENCE, strlen(payload) + 1, payload );
+                        Packet* responseOuterPkt = UDPPacket::create(
+                            self, senderHap, responseInnerPkt->c_str_length(), responseInnerPkt->c_str());
+                        sock.sendPacket(responseOuterPkt);
+                    }
+                }
         
-        usleep(static_cast<int>(POLLING_RATE_IN_SECONDS * 1000000));
+            usleep(static_cast<int>(POLLING_RATE_IN_SECONDS * 1000000));
+            }
         }
-    }
-    catch(Error & e){
-        cout << e.getMsg() << endl;
-        //listenSock.closeSocket();
+        catch(Error & e){
+            cout << e.getMsg() << endl;
+            //listenSock.closeSocket();
+        }
+        usleep(static_cast<int>( RETRY_RATE_IN_SECONDS * 1000000));
     }
     return 0;
 }
