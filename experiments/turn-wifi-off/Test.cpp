f64a451bb498f6b46a32774958e150cb8d629043
Stephen Brown <StephenBrownCS@gmail.com>
2013-12-08 12:57:34 -0600

    experiments: back-ported current versions of BigBuckSensingNode wifi
    I moved code from BigBuckSensingNode into experiments, to facilitate the
    testing of those changes in isolation.

--- experiments/turn-wifi-off/Test.cpp
+++ experiments/turn-wifi-off/Test.cpp
@@ -1,11 +1,31 @@
 #include <cstdlib>
 #include <iostream>
+#include <chrono>
+#include <thread>
 
+using std::this_thread::sleep_for;
+using std::chrono::milliseconds;
 using namespace std;
 
 int main(){
 
-    system("ifdown wlan0");
+    // Can do this all with one system command - using ifdown, which turns off 
+    // the wifi driver
+    int ret = -1;
+    int numTries = 0;
+    while ( ret < 0 ){
+        ret = system("ifdown wlan0");
+        if ( ret < 0 ){
+            cout << "turn off failed" << endl;
+            sleep_for(milliseconds( 5 * 1000 ));
+        } 
+        
+        numTries++;
+        if ( numTries > NUM_TRIES_TO_TURN_OFF_WIFI){
+            throw Error("Unable to turn off wlan0 using ifdown");
+        }
+    }
+    cout << "Successfully shut down wifi" << endl;
 
     return 0;
 }
--- experiments/turn-wifi-off/Test2.cpp
+++ experiments/turn-wifi-off/Test2.cpp
@@ -8,6 +8,57 @@ using std::chrono::milliseconds;
 using namespace std;
 
 int main(){
+    bool wifiInterfaceIsUp = false;
+    int ret = 0;
+    int numTries = 0;
+    while ( ! wifiInterfaceIsUp ){
+        cout << "ifup wlan0" << endl;
+        ret = system("ifup wlan0");
+        cout << "... returned " << ret << endl;
+        wifiInterfaceIsUp = ret >= 0;
+        if ( !wifiInterfaceIsUp ){
+            sleep_for(milliseconds( MILLISECONDS_TO_WAIT_AFTER_IFCONFIG_FAILURE ));
+        }
+        
+        numTries++;
+        if ( numTries > NUM_TRIES_TO_TURN_ON_WIFI){
+            throw Error("Unable to turn on wifi driver using ifup");
+        }
+    }
+
+    // STEP TWO: TURN OFF WLAN0 INTERFACE TO RESET IT
+    numTries = 0;
+    ret = -1;
+    while (ret < 0){
+        cout << "ifconfig wlan0 down";
+        ret = system("ifconfig wlan0 down");
+        cout << "... returned " << ret << endl;
+        if ( ret < 0 ){
+            sleep_for(milliseconds( MILLISECONDS_TO_WAIT_AFTER_IFCONFIG_FAILURE ));
+        }
+        numTries++;
+        if ( numTries > NUM_TRIES_TO_TURN_ON_WIFI){
+            throw Error("Unable to reset wlan0 using ifconfig wlan0 down");
+        }
+    }
+
+    // STEP THREE: TURN ON WLAN0 INTERFACE
+    numTries = 0;
+    ret = -1;
+    while ( ret < 0){
+        cout << "ifconfig wlan0 up" << endl;
+        ret = system("ifconfig wlan0 up");
+        cout << "... returned " << ret << endl;
+        if ( ret < 0 ){
+            sleep_for(milliseconds( MILLISECONDS_TO_WAIT_AFTER_IFCONFIG_FAILURE0 ));
+        }
+        numTries++;
+        if ( numTries > NUM_TRIES_TO_TURN_ON_WIFI){
+            throw Error("Unable to enable wlan0 using ifconfig wlan0 up");
+        }
+    }
+
+/*
     cout << "ifup wlan0" << endl;
     
     bool wifiInterfaceIsUp = false;
@@ -33,7 +84,7 @@ int main(){
     cout << "ifconfig wlan0 up" << endl;
     ret = system("ifconfig wlan0 up");
     cout << "... returned " << ret << endl;
-   
+*/
 
     return 0;
 }
